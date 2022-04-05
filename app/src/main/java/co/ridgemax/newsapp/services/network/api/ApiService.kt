a960e57889106186242f50d2001402e14b9793ee
slavzilla <skovacevic96@outlook.com>
2022-04-05 11:05:13 +0200

    Rearrange API calls

--- app/src/main/java/co/ridgemax/newsapp/services/network/api/ApiService.kt
+++ app/src/main/java/co/ridgemax/newsapp/services/network/api/ApiService.kt
@@ -1,7 +1,9 @@
 package co.ridgemax.newsapp.services.network.api
 
 
+import co.ridgemax.newsapp.modules.article.models.NewsResponse
 import co.ridgemax.newsapp.modules.user.models.User
+import co.ridgemax.newsapp.utils.Constants
 import retrofit2.Response
 import retrofit2.http.*
 
@@ -19,4 +21,24 @@ interface ApiService {
 
     @POST("auth/logout")
     suspend fun logout(): Response<ApiData<User>>
+
+    @GET("v2/top-headlines")
+    suspend fun getTopNews(
+        @Query("country")
+        country: String = "us",
+        @Query("page")
+        pageNo : Int = 1,
+        @Query("apiKey")
+        apiKey: String = Constants.API_KEY
+    ) : Response<NewsResponse>
+
+    @GET("v2/everything")
+    suspend fun search(
+        @Query("q")
+        query: String,
+        @Query("page")
+        pageNo : Int = 1,
+        @Query("apiKey")
+        apiKey: String = Constants.API_KEY
+    ) : Response<NewsResponse>
 }
