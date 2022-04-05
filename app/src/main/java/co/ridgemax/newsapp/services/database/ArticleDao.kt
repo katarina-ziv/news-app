2e74adb1d30eb3679b3a3424db96c3aa9fe72efa
katarina <kat_ziv@yahoo.com>
2022-04-05 14:10:45 +0200

    Room and Dao setup

--- app/src/main/java/co/ridgemax/newsapp/services/database/ArticleDao.kt
+++ app/src/main/java/co/ridgemax/newsapp/services/database/ArticleDao.kt
@@ -1,8 +1,18 @@
 package co.ridgemax.newsapp.services.database
 
-import androidx.room.Dao
+import androidx.room.*
+import co.ridgemax.newsapp.modules.article.models.Article
+import kotlinx.coroutines.flow.StateFlow
 
 @Dao
 interface ArticleDao {
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(article: Article): Long
+
+    @Query("SELECT * FROM articles")
+    fun getAllArticles() : StateFlow<List<Article>>
+
+    @Delete
+    suspend fun deleteArticle(article: Article)
 }
